83d6dae0416f869781b94b318b193452c0f5d52f
hessouis <hessouis@LFR019906.corp.capgemini.com>
2017-12-29 01:16:47 +0100

    poule details

--- src/pages/poule-details/poule-details.ts
+++ src/pages/poule-details/poule-details.ts
@@ -1,6 +1,6 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {ActionSheetController, NavController, NavParams, Platform, ToastController} from 'ionic-angular';
-import {GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, LatLng } from '@ionic-native/google-maps';
+import {GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, Marker, GoogleMapsAnimation, MyLocation } from '@ionic-native/google-maps';
 
 import { RencontresService } from '../../providers/rencontres/rencontres-service';
 
@@ -13,8 +13,8 @@ import { RencontresService } from '../../providers/rencontres/rencontres-service
 export class PouleDetailsPage {
 
   rencontre: any;
+  mapReady: boolean = false;
   map: GoogleMap;
-  mapElement: HTMLElement;
   
         constructor(public actionSheetCtrl: ActionSheetController, public navCtrl: NavController, public navParams: NavParams, public rencontreService: RencontresService, public toastCtrl: ToastController, 
     		private googleMaps: GoogleMaps, public platform: Platform) {
@@ -22,18 +22,14 @@ export class PouleDetailsPage {
 	        rencontreService.findById(this.rencontre.id).then(
 	            rencontre => this.rencontre = rencontre
             );
-           // Wait the native plugin is ready.
-		    platform.ready().then(() => {
-		      this.loadMap();
-		   });
     }
+    
 
   ionViewDidLoad() {
     this.loadMap();
   }
 
-  loadMap() {
-    this.mapElement = document.getElementById('map');
+   loadMap() {
 
     let mapOptions: GoogleMapOptions = {
       camera: {
@@ -46,12 +42,13 @@ export class PouleDetailsPage {
       }
     };
 
-    //this.map = this.googleMaps.create(this.mapElement, mapOptions);
-	this.map = new GoogleMap(this.mapElement, mapOptions);
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
     // Wait the MAP_READY before using any methods.
     this.map.one(GoogleMapsEvent.MAP_READY)
       .then(() => {
         console.log('Map is ready!');
+        this.mapReady = true;
 
         // Now you can use all methods safely.
         this.map.addMarker({
@@ -71,5 +68,51 @@ export class PouleDetailsPage {
           });
 
       });
+  }
+  onButtonClick() {
+    if (!this.mapReady) {
+      this.showToast('map is not ready yet. Please try again.');
+      return;
+    }
+    this.map.clear();
+
+    // Get the location of you
+    this.map.getMyLocation()
+      .then((location: MyLocation) => {
+        console.log(JSON.stringify(location, null ,2));
+
+        // Move the map camera to the location with animation
+        return this.map.animateCamera({
+          target: location.latLng,
+          zoom: 17,
+          tilt: 30
+        }).then(() => {
+          // add a marker
+          return this.map.addMarker({
+            title: '@ionic-native/google-maps plugin!',
+            snippet: 'This plugin is awesome!',
+            position: location.latLng,
+            animation: GoogleMapsAnimation.BOUNCE
+          });
+        })
+      }).then((marker: Marker) => {
+        // show the infoWindow
+        marker.showInfoWindow();
+
+        // If clicked it, display the alert
+        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+          this.showToast('clicked!');
+        });
+      });
+  }
+
+  showToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'middle'
+    });
+
+    toast.present(toast);
   }
 }
